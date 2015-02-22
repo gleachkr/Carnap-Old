7b3f48cc2abaed9f7f1f126a70ce95e53983a493
gleachkr <gleachkr@ksu.edu>
2015-02-22 15:24:54 -0600

    Added handling for subproofs, fixed some minor errors.

--- ProofTreeHandler.hs
+++ ProofTreeHandler.hs
@@ -10,28 +10,35 @@ import AbstractDerivationDataTypes
 --of, or into a line-by-line list of errors
 
 --------------------------------------------------------
---Main processing functions
+--1. Main processing functions
 --------------------------------------------------------
 
 type ErrorList = [String]
 
---The proof tree is first converted into a derivation that reflects the
+--The proof forest is first converted into a derivation that reflects the
 --actual structure of the argument. We get an errorlist here if the
---prooftree doesn't actually describe a derivation (because a rule is
+--ProofForest doesn't actually describe a derivation (because a rule is
 --getting the wrong number of premises or something).
 --
 --The resulting derivation is then checked for correctness. We get an
 --errorlist here if there are some steps that aren't correct, for example
 --if a rule is applied in such a way that the conclusion does not follow.
-handleTree :: ProofTree -> Either ErrorList Argument
-handleTree t = do d <- treeToDerivaton t
-                  derivationProves d
+handleForest :: ProofForest -> Either ErrorList Argument
+handleForest t = do d <- treeToDerivaton t
+                    derivationProves d
 
---this runs a ProofTree through a tree-processing function that returns two
+
+--------------------------------------------------------
+--1.1 Tree and Forest to derivation functions
+--------------------------------------------------------
+--These are functions that are collectively resonsible for turning
+--a ProofForest into PropositionalDerivation, that can then be checked.
+
+--this runs a ProofForest through a processing function that returns two
 --lists: an errorlist, and a list of what derivations are constructed on
 --each line. It cleans this output, and returns what's needed for the
---tree-handler
-treeToDerivatonList :: ProofTree -> Either ErrorList PropDerivation
+--Forest-Handler
+forestToDerivatonOrList :: ProofTree -> Either ErrorList PropDerivation
 
 --this processes a ProofTree by building up a list of judgements that have
 --been successfully constructed on each line, and of errors in attempted
@@ -43,6 +50,20 @@ treeProcessor (Node (Right line) []) el dl = assertionProcessor line el dl
 treeProcessor (Node (Right line) f) el dl = subProofProcessor line f el dl
 --I don't think this last case can arise
 treeProcessor (Node (Left err) f) el dl = undefined
+
+--this processes a ProofForest by folding together the errorlists and
+--derivationlists that arise from its individual trees.
+forestProcessor :: ProofForest -> ErrorList -> [Maybe PropositionalJudgement] -> 
+                    (ErrorList, [Maybe PropositionalJudgement])
+forestProcessor forest el dl = foldl combineWithTree (el, dl) forest
+    where combineWithTree (el',dl') t =  treeProcessor t el' dl'
+
+
+--------------------------------------------------------
+--1.1.1 Assertion Processing 
+--------------------------------------------------------
+--These are functions that are responsible for handling single-assertion
+--lines
 
 --this processes a line containing a well-formed assertion, in the context
 --of an errorlist and a list of possibly completed judgments.
@@ -68,4 +89,34 @@ retrieveTwo l1 l2 dl = if length dl > max l1 l2
                            then Just (reverse dl !! (l1 - 1), reverse dl !! (l2 -1))
                            else Nothing
 
+--------------------------------------------------------
+--1.1.2 Subproof processing
+--------------------------------------------------------
+--These are functions that are responsible for handling subproofs.
 
+--XXX:there are several repeated applications of forestProcessor here. this
+--could be a lot less redundant.
+
+--This function takes a line that introduces a ProofForest, and adjusts the
+--ErrorList and the list of subderivations accordingly
+subProofProcessor line forest el dl = case line of
+                                          (f, SHOW, _) -> 
+                                                closeFrom (length $ el + 1) $ forestProcessor "Open Subproof":el Nothing:dl forest
+                                          (f, CP, l) -> 
+                                                closeFrom (length $ el + 1) $ unaryTerminationHandler forest f CP l el dl
+
+closeFrom l (el,dl) = (el, close l dl)
+    where close l' dl' = take l dl ++ map (\x -> Nothing) (drop l dl)
+
+unaryTerminationHandler forest f r l el dl = case l of 
+                                                [l1] -> closeWith forest f l1 r el dl
+                                                _ -> forestProcessor forest ("wrong number of lines cited":el) (Nothing:dl)
+
+closeWith forest f l1 r el dl = case retrieveOne l1 forest el dl of 
+                                    Nothing -> (forestProcessor forest ("unavailable line":el) (Nothing:dl))
+                                    (Just j) -> forestProcessor forest ("":el) (j:dl)
+
+retrieveOne l1 forest el dl = if length preProof > l1
+                                  then Just (reverse preProof !! l1)
+                                  else Nothing
+                            where preProof = forestProcessor forest ("":el) (Nothing:dl)
