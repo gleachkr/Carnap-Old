304765cdecc9c1202c62d398c960d2d775355b24
gleachkr <gleachkr@ksu.edu>
2015-02-23 13:41:02 -0600

    More refactoring and documentation

--- PropositionalLanguage.hs
+++ PropositionalLanguage.hs
@@ -4,15 +4,48 @@ module PropositionalLanguage where
 
 import AbstractSyntaxDataTypes
 
+
+--------------------------------------------------------
+--1. Data types for a simple propositional language
+--------------------------------------------------------
+--These are datatypes for a propositional language
+
+--------------------------------------------------------
+--1.1 Boolean Sentences
+--------------------------------------------------------
+
+--atomic sentences carry boolean values (relative to an assignment)
+data BooleanSentence a where
+        Sentence :: {index :: Int} -> BooleanSentence Bool
+
+--where assignments are nothing but functions from (indicies of the) the
+--apprpriate BooleanSentences to truth values.
 type Assignment = Int -> Bool
 
+--an atomic sentence is true in a valuation iff that valuation assigns it
+--"True".
+instance Modelable BooleanSentence Assignment where
+        satisfies v (Sentence n) = v n
+
+instance Eq (BooleanSentence a) where
+        Sentence x == Sentence y = x == y
+
+instance Schematizable BooleanSentence where
+        schematize (Sentence n) _ = "P_" ++ show n
+        
+--------------------------------------------------------
+--1.2 Boolean connectives
+--------------------------------------------------------
+
+--connectives carry functions from boolean values to boolean values.
 data BooleanConnectives a where
         Not :: BooleanConnectives (Bool -> Bool)
         And :: BooleanConnectives (Bool -> Bool -> Bool)
         Or  :: BooleanConnectives (Bool -> Bool -> Bool)
         If  :: BooleanConnectives (Bool -> Bool -> Bool)
         Iff :: BooleanConnectives (Bool -> Bool -> Bool)
-
+        
+--the functions they carry are invariant across assingments.
 instance Modelable BooleanConnectives Assignment where
         satisfies _ Not = not
         satisfies _ And = (&&)
@@ -42,27 +75,25 @@ instance Schematizable BooleanConnectives where
                                          _   -> undefined
         schematize Iff = \x -> case x of [y,z] -> "(" ++ y ++ " iff " ++ z ++ ")"
                                          _   -> undefined
+                                         
 
-data BooleanSentence a where
-        Sentence :: {index :: Int} -> BooleanSentence Bool
-
-instance Eq (BooleanSentence a) where
-        Sentence x == Sentence y = x == y
-
-instance Schematizable BooleanSentence where
-        schematize (Sentence n) = \_ -> "P_" ++ show n
-
-instance Modelable BooleanSentence Assignment where
-        satisfies v (Sentence n) = v n
-
+--------------------------------------------------------
+--1.3 Propositional Formulas
+--------------------------------------------------------
+--a propositional formula is built from:
 type PropositionalFormula = Form Nothing --no predicates
                                 BooleanConnectives --boolean connectives
                                 Nothing --no quantifiers
                                 Nothing --no function symbols
-                                BooleanSentence --semantic values are boolean sentences
+                                BooleanSentence 
+                                    --semantic values are BooleanSentences
+                                    --(intuitively, intensions or fregean
+                                    --senses: things that let you compute
+                                    --the reference of something in a given
+                                    --model.)
                                 Bool --only types are booleans
 
---for some reason it's happy to use the instance of Eq with
+--XXX: for some reason it's happy to use the instance of Eq with
 --BooleanSentence, but not with BooleanConnectives. I think it may be that
 --the pattern match on the left gives it enough information to sign off on
 --the equality definition on the right. 
@@ -73,6 +104,10 @@ instance Eq PropositionalFormula where
         BinaryConnect  If x y == BinaryConnect  If z w = x == z && y == w
         BinaryConnect Iff x y == BinaryConnect Iff z w = x == z && y == w
         _ == _ = False
+
+--------------------------------------------------------
+--2. Wrapper functions for constructors
+--------------------------------------------------------
 
 lneg :: PropositionalFormula -> PropositionalFormula
 lneg = UnaryConnect Not
@@ -88,6 +123,8 @@ lif = BinaryConnect If
 
 (.→.) :: PropositionalFormula -> PropositionalFormula -> PropositionalFormula
 (.→.) = lif 
+
+--TODO: Some additional infix wrappers would be nice.
 
 pn :: Int -> PropositionalFormula
 pn n = ConstantFormBuilder (Sentence n)
