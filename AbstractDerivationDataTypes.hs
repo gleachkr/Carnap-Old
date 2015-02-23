7a784376d5994f017b4917dcca6bc6e365b31dc8
gleachkr <gleachkr@ksu.edu>
2015-02-23 12:52:12 -0600

    Additional comments on abstract DerivationDataTypes

--- AbstractDerivationDataTypes.hs
+++ AbstractDerivationDataTypes.hs
@@ -1,37 +1,42 @@
-
----vim:fdm=marker
 {-#LANGUAGE GADTs, TypeSynonymInstances, FlexibleInstances, FlexibleContexts, MultiParamTypeClasses #-}
 
 module AbstractDerivationDataTypes where 
 
 import Control.Monad.Identity
 
-                            
---TODO: Monad Instance.
--- Looks like you might just be able to use the identity monad, with infix
--- line constructors, so that it looks like
---
--- l4 <- phi1 land phi2 byMP l2 l3
+--This module contains some datatypes for derivations, at a level of
+--abstraction where they will hopefully be applicable to many particuar
+--deductive systems
 
+--------------------------------------------------------
+--Data Types for Abstract Derivatons
+--------------------------------------------------------                            
+
+--A judgement is a basic unit of derivation. It contains a formula, which
+--is asserted, and a justification, which will be, in the propositional
+--case, usually something constructed by applying an inference rule to
+--several other judgements. In more general cases, a justification might
+--contain other things, e.g. a context relative to which the assertion is
+--being made. 
 data Judgement contents justification where
                             Line :: {lineContents :: contents, lineJustification :: justification} 
                                 -> Judgement contents justification
 
+--Derivation is a wrapper type using the identity monad that makes it easy
+--to use monadic 'do' syntax to construct judgements.
 type Derivation = Identity
 
+--this introduces a new judgement into a derivation.
 assert :: Judgement a b -> Derivation (Judgement a b)
 assert = Identity
 
+--this extracts the final judgement from a derivation.
 conclusion :: Derivation (Judgement a b) -> Judgement a b
 conclusion = runIdentity
 
+--this takes a derivation containing a pair of a derived judgement, and
+--something that constructs justifications from judgements (e.g. the
+--conditional proof constructor), and unwraps both of them (probably so
+--that you can apply the second to the first.
 subproof :: Derivation (Judgement a b, Judgement a b -> b) -> (Judgement a b, Judgement a b -> b)
 subproof = runIdentity
-
---TODO: show, equality instances.
-
---TODO: test for correct derivations. Could this be done using blanks to
---deal with variable binding situations?
-
-
-
