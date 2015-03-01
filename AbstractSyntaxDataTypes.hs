28213ac7fe3ad099839820fe89bfebf392f08a9a
gleachkr <gleachkr@ksu.edu>
2015-03-01 14:18:27 -0600

    Prototype Unifiable Instance for SchematicTerm

--- AbstractSyntaxDataTypes.hs
+++ AbstractSyntaxDataTypes.hs
@@ -264,6 +264,24 @@ instance (UniformlyEq f, UniformlyEq sv, Schematizable sv, Schematizable f) =>
         --everything else counts as failure to match
         match _ _ = Nothing
 
+instance (UniformlyEq f, UniformlyEq sv, Schematizable sv, Schematizable f) =>
+        Unifiable SSymbol (SchematicTerm f sv ()) where
+
+        matchVar (S_ConstantSchematicTermBuilder c) t = Just $ (c,t)
+        matchVar (S_UnarySchematicFuncApp f S_BlankTerm) t@(S_UnaryFuncApp f' S_BlankTerm) 
+            = Just $ (f,t) 
+        matchVar (S_UnarySchematicFuncApp f S_BlankTerm ) t@(S_UnarySchematicFuncApp f' S_BlankTerm) 
+            = Just $ (f,t) 
+        matchVar (S_BinarySchematicFuncApp f S_BlankTerm S_BlankTerm) t@(S_BinaryFuncApp f' S_BlankTerm S_BlankTerm) 
+            = Just $ (f,t) 
+        matchVar (S_BinarySchematicFuncApp f S_BlankTerm S_BlankTerm ) t@(S_BinarySchematicFuncApp f' S_BlankTerm S_BlankTerm) 
+            = Just $ (f,t)
+
+        makeTerm symb@(C,_) = S_ConstantSchematicTermBuilder symb
+        makeTerm symb@(F1,_) = S_UnarySchematicFuncApp symb S_BlankTerm
+        makeTerm symb@(F2,_) = S_BinarySchematicFuncApp symb S_BlankTerm S_BlankTerm
+        makeTerm _ = S_BlankTerm
+
 --------------------------------------------------------
 --2.1 Abstract Formulas
 --------------------------------------------------------
