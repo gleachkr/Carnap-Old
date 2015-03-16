c4d753fbbabea66e6fe59386265fb18cba5810e3
gleachkr <gleachkr@ksu.edu>
2015-03-16 14:28:39 -0500

    More reorganization

--- AbstractSyntaxUnification.hs
+++ AbstractSyntaxUnification.hs
@@ -11,7 +11,133 @@ import Data.Map as Map
 --the level of abstract syntax.
 
 --------------------------------------------------------
---1. Unification for Schematic Terms
+--1. Generalities
+--------------------------------------------------------
+
+--------------------------------------------------------
+--1.1 Schematic Versions of Syntax Typeclasses
+--------------------------------------------------------
+
+class S_NextVar sv quant where
+        s_appropriateVariable :: SchematicForm pred con quant f sv () -> quant ((sv b -> c) -> a) -> String
+
+--similarly, for schematic quantifiers
+class SchemeVar sv where
+        appropriateSchematicVariable :: SchematicForm pred con quant f sv c -> SSymbol -> String
+
+--a class for types of things to which we want to associate strings with
+--multiple blanks. For example:
+--
+--  1. terms with occurances of the blank term
+--  2. function and connective symbols that we might want to write inline,
+--  or with various sorts of parentheses
+
+instance S_NextVar a Nothing where
+        s_appropriateVariable _ _ = undefined
+
+--------------------------------------------------------
+--1.1 Schematic Symbols
+--------------------------------------------------------
+
+data SynType = C | --constant symbol
+              F1 | --unary and binary function symbol
+              F2 |
+               S | --sentence letter
+              P1 | --unary and binary predicate symbol
+              P2 | 
+             CN1 | --unary and binary connective
+             CN2 |
+               Q | --quantifier
+              PL --premise list, as in a sequent
+             deriving (Show, Eq, Ord)
+
+type SSymbol = (SynType, String)
+
+--It might be useful to have tags to keep schematic symbols that stand for
+--objects of different syntactic categories separate.
+
+synType :: (a, b) -> a
+synType = fst
+
+shapeOfSymbol :: (a, b) -> b
+shapeOfSymbol = snd
+
+--TODO: ultimately, this should be eliminated, and the type of schematic
+--symbol should be part of the specification of the schematic langage type.
+--This might muck up the functional dependency of schematic language on
+--language.
+
+--------------------------------------------------------
+--2. Unification for Schematic Terms
+--------------------------------------------------------
+
+--------------------------------------------------------
+--2.1 Abstract Term Schemata
+--------------------------------------------------------
+
+--these are schematic abstract terms, i.e. terms with schematic variables
+--(as opposed to non-schematic variables of the type that can be bound by
+--quantifiers in the language itself) in them. These schematic variables
+--are useful for unification
+--
+--f is a type of function symbols, sv is a semantic-value type (intutively,
+--fregean intensions denoting whatever sv wraps)
+--
+--The naming convention is that the things that are in effect just concrete
+--symbols get names prefixed by S_ or s_ and that's all. The constructors
+--for actual variables get names with Schematic before the first now. the
+--selectors that are definitely pulling out variables get scheme as their
+--first word.
+--
+--Because we will not eval Schematic terms, we throw away information about
+--semantic types; this simplifies matching later on.
+data SchematicTerm f sv a where 
+        S_BlankTerm                    ::   SchematicTerm f sv ()
+        S_ConstantTermBuilder          ::   {      s_cVal :: sv a } -> SchematicTerm f sv ()
+        S_ConstantSchematicTermBuilder ::   {  schemeBVal :: SSymbol } -> SchematicTerm f sv ()
+        S_UnaryFuncApp                 ::   {     s_uFunc :: f ( b -> a ) , 
+                                                  s_uTerm :: SchematicTerm f sv () } -> SchematicTerm f sv ()
+        S_UnarySchematicFuncApp        ::   { schemeUFunc :: SSymbol , 
+                                                  s_uTerm :: SchematicTerm f sv () } -> SchematicTerm f sv ()
+        S_BinaryFuncApp                ::   {     s_bFunc :: f ( b -> c -> a), 
+                                                 s_bTerm1 :: SchematicTerm f sv (), 
+                                                 s_bTerm2 :: SchematicTerm f sv ()} -> SchematicTerm f sv ()
+        S_BinarySchematicFuncApp       ::   { schemeBFunc :: SSymbol , 
+                                                 s_bTerm1 :: SchematicTerm f sv (), 
+                                                 s_bTerm2 :: SchematicTerm f sv ()} -> SchematicTerm f sv ()
+--XXX:Reduplication is ugly.
+
+instance Scheme (Term f sv a) (SchematicTerm f sv ()) where
+        liftToScheme (ConstantTermBuilder c) = S_ConstantTermBuilder c
+        liftToScheme (UnaryFuncApp f t) = S_UnaryFuncApp f $ liftToScheme t
+        liftToScheme (BinaryFuncApp f t1 t2) 
+            = S_BinaryFuncApp f (liftToScheme t1) (liftToScheme t2)
+        liftToScheme BlankTerm = undefined
+                            
+--TODO: Eventually we should add a "schematic symbol type" as a new parameter
+--to Schematic terms and formulas. When that's done, this can be made more
+--generic, by introducing typeclass for concrete types that we want to
+--associated a schematization function to, and requiring that the schematic
+--symbol type be a member of that class.
+instance (Schematizable sv, Schematizable f) => Schematizable ( SchematicTerm f sv ) where
+        schematize (S_ConstantTermBuilder x) = \_ -> schematize x [] 
+        schematize (S_ConstantSchematicTermBuilder x) = \_ -> shapeOfSymbol x
+        schematize (S_UnaryFuncApp f x) = \y -> schematize f [schematize x y]
+        schematize (S_UnarySchematicFuncApp f x) 
+            = \y -> shapeOfSymbol f ++ "(" ++ schematize x y ++ ")"
+        schematize (S_BinaryFuncApp f x y) 
+            = \z -> schematize f [schematize x z , schematize y z]
+        schematize (S_BinarySchematicFuncApp f x y) 
+            = \z -> shapeOfSymbol f ++ "(" ++ schematize x z ++ "," ++ schematize y z ++ ")"
+
+instance Schematizable (SchematicTerm f sv) => Show (SchematicTerm f sv a) where
+        show x = schematize x ["_"] --inserts a literal blank for semantic blanks. 
+
+instance Schematizable (SchematicTerm f sv) => Eq (SchematicTerm f sv a) where
+        (==) t1 t2 = show t1 == show t2
+
+--------------------------------------------------------
+--2.2 Unfication Instances
 --------------------------------------------------------
 
 instance (Schematizable f, Schematizable sv) => 
@@ -103,10 +229,122 @@ instance (UniformlyEq f, UniformlyEq sv, Schematizable sv, Schematizable f) =>
         makeTerm symb@(F2,_) = S_BinarySchematicFuncApp symb S_BlankTerm S_BlankTerm
         makeTerm _ = S_BlankTerm
 
+--------------------------------------------------------
+--3. Unification for Schematic Forms
+--------------------------------------------------------
 
 --------------------------------------------------------
---2. Unification for Schematic Forms
+--3.1 Abstract Formula Schemata
 --------------------------------------------------------
+
+--The naming convention is that the things that are in effect just concrete
+--symbols get names prefixed by S_ or s_ and that's all. The constructors
+--for actual variables get names with Schematic before the first now. the
+--selectors that are definitely pulling out variables get scheme as their
+--first word.
+--
+--NOTE: There's a kind of subtle thing here. Should our schematic variables be
+--semantic type agnostic---so that they can match concrete symbols of any
+--semantic type---or should they themselves be semantically typed? In
+--actual practice, I think the type-agnostic form of scheme is definitely
+--the standard thing. But why shouldn't there sematically-typed schematic
+--variables?
+--
+--As with Schematic terms, we throw away semantic information that is no
+--longer relevant, in order to make substitution easier.
+data SchematicForm pred con quant f sv a where
+        S_BlankForm                          :: SchematicForm pred con quant f sv ()
+        S_ConstantFormBuilder                :: {s_pVal            :: sv a}
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_ConstantSchematicFormBuilder       :: {schemePVal        :: SSymbol}
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_UnaryPredicate                     :: { s_uPred          :: pred (b -> a),
+                                                s_uSub             :: SchematicTerm f sv ()}
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_UnarySchematicPredicate            :: { schemeUPred      :: SSymbol,
+                                                s_uSub             :: SchematicTerm f sv ()}
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_BinaryPredicate                    :: { s_bPred          :: pred (b -> c -> a),
+                                                s_bSub1            :: SchematicTerm f sv (),
+                                                s_bSub2            :: SchematicTerm f sv ()}
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_BinarySchematicPredicate           :: { schemeBPred      :: SSymbol,
+                                                s_bSub1            :: SchematicTerm f sv (),
+                                                s_bSub2            :: SchematicTerm f sv ()}
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_UnaryConnect                       :: { s_uConn          :: con (b -> a),
+                                                s_uForm            :: SchematicForm pred con quant f sv () } 
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_UnarySchematicConnect              :: { schemeUConn      :: SSymbol,
+                                                s_uForm            :: SchematicForm pred con quant f sv () } 
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_BinaryConnect                      :: { s_bConn          :: con (b -> c -> a),
+                                                s_bForm1           :: SchematicForm pred con quant f sv (),
+                                                s_bForm2           :: SchematicForm pred con quant f sv () }
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_BinarySchematicConnect             :: { schemeBConn      :: SSymbol,
+                                                s_bForm1           :: SchematicForm pred con quant f sv (),
+                                                s_bForm2           :: SchematicForm pred con quant f sv () }
+                                                                   -> SchematicForm pred con quant f sv ()
+        S_Bind                               :: { s_quantifier     :: quant ((sv b ->c) -> a) ,
+                                                s_quantified       :: Term f sv d -> SchematicForm pred con quant f sv ()
+                                                }                  -> SchematicForm pred con quant f sv ()
+        S_SchematicBind                      :: { schemeQuantifier :: SSymbol,
+                                                s_quantified       :: Term f sv d -> SchematicForm pred con quant f sv ()
+                                                }                  -> SchematicForm pred con quant f sv ()
+--XXX: Reduplication is ugly. Possible Solution: dispense with Form, and
+--simply work with schematic formulas all the time.
+
+instance Scheme ( Form pred con quant f sv a ) ( SchematicForm pred con quant f sv () ) where
+        liftToScheme (ConstantFormBuilder c) = S_ConstantFormBuilder c
+        liftToScheme (UnaryPredicate p t) = S_UnaryPredicate p $ liftToScheme t
+        liftToScheme (BinaryPredicate p t1 t2) = S_BinaryPredicate p (liftToScheme t1) (liftToScheme t2)
+        liftToScheme (UnaryConnect c f) = S_UnaryConnect c $ liftToScheme f
+        liftToScheme (BinaryConnect c f1 f2) = S_BinaryConnect c (liftToScheme f1) (liftToScheme f2)
+        liftToScheme (Bind q q'ed) = S_Bind q $ \x -> liftToScheme (q'ed x)
+        liftToScheme BlankForm = S_BlankForm
+
+--XXX: This gets around schematizing schematic variables (which themselves
+--don't have a schematizable instance) in a kind of quick and dirty way.
+--Ultimately, this should be done better.
+instance (Schematizable pred, Schematizable con, Schematizable quant, Schematizable sv,
+        Schematizable f, S_NextVar sv quant, SchemeVar sv)
+        => Schematizable (SchematicForm pred con quant f sv) where
+                schematize (S_ConstantFormBuilder x) = \_ -> schematize x []
+                schematize (S_ConstantSchematicFormBuilder x) = \_ -> shapeOfSymbol x 
+                schematize (S_UnaryPredicate p t) = 
+                    \y -> schematize p [schematize t y]
+                schematize (S_UnarySchematicPredicate p t) = 
+                    \y -> shapeOfSymbol p ++ "(" ++ schematize t y ++ ")"
+                schematize (S_BinaryPredicate p t1 t2) = 
+                    \y -> schematize p [schematize t1 y, schematize t2 y]
+                schematize (S_BinarySchematicPredicate p t1 t2) = 
+                    \y -> shapeOfSymbol p ++ "(" ++ schematize t1 y ++ "," ++ schematize t2 y ++ ")"
+                schematize (S_UnaryConnect c f) = 
+                    \y -> schematize c [schematize f y]
+                schematize (S_UnarySchematicConnect c f) = 
+                    \y -> shapeOfSymbol c ++ schematize f y
+                schematize (S_BinaryConnect c f1 f2) = 
+                    \y -> schematize c [schematize f1 y,schematize f2 y]
+                schematize (S_BinarySchematicConnect c f1 f2) = 
+                    \y -> "(" ++ schematize f1 y ++ shapeOfSymbol c ++ schematize f2 y ++ ")"
+                schematize (S_Bind q f) = 
+                    \_ -> schematize q [s_appropriateVariable (f BlankTerm) q, 
+                        schematize (f BlankTerm) [s_appropriateVariable (f BlankTerm) q]]
+                schematize (S_SchematicBind q f) = 
+                    \_ -> shapeOfSymbol q ++ appropriateSchematicVariable (f BlankTerm) q ++ "(" ++
+                        schematize (f BlankTerm) [appropriateSchematicVariable (f BlankTerm) q] ++ ")"
+                schematize _ = \_ -> ""
+
+instance Schematizable (SchematicForm pred con quant f sv) => Show (SchematicForm pred con quant f sv a) where
+        show x = schematize x ["_"] --inserts a literal blank for semantic blanks. 
+
+instance Schematizable (SchematicForm pred con quant f sv) => Eq (SchematicForm pred con quant f sv a) where
+        (==) f1 f2 = show f1 == show f2
+
+instance Schematizable (SchematicForm pred con quant f sv) => Ord (SchematicForm pred con quant f sv a) where
+        (<=) f1 f2 = show f1 <= show f2
+
 
 
 --does not drive the substititon down to to level of terms. We probably
@@ -243,7 +481,7 @@ instance (UniformlyEq f, UniformlyEq pred, UniformlyEq sv, UniformlyEq con, Unif
         makeTerm symb@(CN2,_) = S_BinarySchematicConnect symb S_BlankForm S_BlankForm 
         
 --------------------------------------------------------
---3. Mixed Unification
+--4. Mixed Unification
 --------------------------------------------------------
 
 type LanguageItem pred con quant f sv a = Either (Form pred con quant f sv a ) (Term f sv a) 
