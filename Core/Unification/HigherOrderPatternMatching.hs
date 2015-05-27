25db671894243c99c3b5a9f8f5db52fe0010ff71
JakeEhrlich <jake.h.ehrlich@gmail.com>
2015-05-26 21:06:19 -0500

    Added stuff that I thought I had already added

---
+++ Core/Unification/HigherOrderPatternMatching.hs
@@ -0,0 +1,152 @@
+{-#LANGUAGE GADTs, FlexibleInstances, MultiParamTypeClasses, FunctionalDependencies, UndecidableInstances, RankNTypes #-}
+
+module Carnap.Core.Unification.HigherOrderPatternMatching () where
+
+import Data.List
+
+--------------------------------------------------------
+--0. We need some helper classes first
+--------------------------------------------------------
+
+--might want to factor this out as it seems like a very useful concept
+class UniformlyEquaitable f where
+    eq :: f a -> f b -> Bool
+    eq x y = not (neq x y)
+    neq :: f a -> f b -> Bool
+    neq x y = not (eq x y)
+
+--------------------------------------------------------
+--1. Define exisitinesal types for homogenous values
+--------------------------------------------------------
+
+--allows for sub parts to be paired up for matching
+data Paring var where
+    Pairing :: Matchable concrete schema var => schema -> concrete -> Paring var
+
+--allows for abitrary kinds of varibles
+data FreeVar var where
+    FreeVar :: (Show (var schema'), UniformlyEquaitable var) => var schema' -> FreeVar var
+
+--like FreeVar except it adds in a schmea' and a list of arguments
+--for the old kind of mappings 
+data Mapping var where
+    LambdaMapping :: (Matchable concrete schema var) => var schema -> [FreeVar var] -> schema -> Mapping var
+
+--just defines a quick alias
+type Subst var = [Mapping var]
+
+--define an Eq instance for FreeVar so that we can use union with it
+--it just falls back on being uniformly equaitable
+instance Eq (FreeVar var) where
+    (FreeVar v1) == (FreeVar v2) = eq v1 v2
+
+instance Show (FreeVar var) where
+    show (FreeVar v) = show v
+
+instance Show (Mapping var) where
+    show (LambdaMapping v [] s) = show v ++ " -> " ++ show s
+    show (LambdaMapping v xs s) = show v ++ " -> lam" ++ concatMap ((" " ++) . show) xs ++ ". " ++ show s
+
+--------------------------------------------------------
+--2. Define the type classes
+--------------------------------------------------------
+
+--this is just to help people out with creating
+--code that performs subtititutions
+--it is not actully used by the unification code
+class EquaitableVar var where
+  --turns the schema' into a schema if the varibles are equal
+  getLikeSchema :: var schema -> var schema' -> schema' -> Maybe schema
+
+instance EquaitableVar var => UniformlyEquaitable var where
+  eq v1 v2 = case getLikeSchema v1 v2 undefined of
+      Just _  -> True  --do not evaluate the argument of this
+      Nothing -> False
+
+--defines how to get free varibles and how to perform substiutions
+class (UniformlyEquaitable var, Show (var schema), Eq schema, Show schema) => Hilbert schema var | schema -> var where
+    freeVars :: schema -> [FreeVar var]
+    apply :: Subst var -> schema -> schema
+
+--finally we need a few more helper terms to define how pattern matching works
+class (Hilbert schema var, Show concrete, Eq concrete) => Matchable concrete schema var | schema -> var concrete where
+    match :: schema -> concrete -> Maybe [Paring var]
+    matchVar :: schema -> concrete -> Maybe (Mapping var)
+    makeTerm :: var schema -> schema
+
+--------------------------------------------------------
+--3. Unification errors
+--------------------------------------------------------
+
+data MatchError var schema where
+    UnableToMatch :: (Show schema, Show concrete) => schema -> concrete -> MatchError var schema
+    ErrWrapper :: Matchable concrete schema' var => MatchError (var schema') schema' -> MatchError (var schema) schema
+    SubError :: (Show schema, Show concrete) => MatchError var schema -> schema -> concrete -> MatchError var schema
+    OccursCheck :: (Show var, Show schema) => var -> schema -> MatchError var schema
+
+instance Show (MatchError var t) where
+    show (UnableToMatch a b) = "Unable to match " ++ show a ++ " with " ++ show b
+    show (ErrWrapper e) = show e
+    show (SubError err a b)  = "When matching " ++ show a ++ " with " ++ show b ++ ",\n" ++ show err
+    show (OccursCheck v t)   = "Cannot construct infinite term " ++ show v ++ " = " ++ show t
+
+--------------------------------------------------------
+--4. Helper functions for unification
+--------------------------------------------------------
+
+--getVarible (Mapping v _) = FreeVar v
+getVarible (LambdaMapping v _ _) = FreeVar v
+
+--somehow this becomes the identity function when doing pattern matching
+applySubToMapping :: Subst var -> Mapping var -> Mapping var
+--applySubToMapping subst (Mapping v c) = Mapping v c
+applySubToMapping subst (LambdaMapping v f s) = LambdaMapping v f s
+
+--this also just becomes concatenation when doing pattern matching
+--we need a way to compose mappings
+(...) :: Subst var -> Subst var -> Subst var
+x ... y = (map (applySubToMapping y) x) ++ y
+
+--maps a function over both elements of a paring
+paringMap :: (forall schema concrete. Matchable concrete schema var => schema -> schema) -> Paring var -> Paring var
+paringMap f (Pairing x y) = Pairing (f x) y
+
+--applySub :: Matchable concrete schema var => Subst var -> schema -> schema
+--applySub subst s = multiApply subst s
+
+--maps a function over a Left
+(Left x) .<. f = Left (f x)
+e        .<. f = e
+
+--maps a function over a right
+(Right x) .>. f = Right (f x)
+e         .>. f = e
+
+--check if a varible is a member of a list of FreeVars
+isMember :: UniformlyEquaitable var => var schema -> [FreeVar var] -> Bool
+isMember v (FreeVar v' : xs) | eq v v' = True
+isMember v (_:xs)                      = isMember v xs
+isMember v []                          = False
+
+--------------------------------------------------------
+--5. Pattern Matching code 
+--------------------------------------------------------
+
+matchChildren :: (Matchable concrete schema var) => [Paring var] -> Either (Subst var) (MatchError (var schema) schema)
+matchChildren ((Pairing a b):xs) = case patternMatch a b of
+    Left subst -> let children = map (paringMap (apply subst)) xs
+                  in (matchChildren children) .<. (subst ...) .>. ErrWrapper
+    Right err  -> Right (ErrWrapper err)
+matchChildren [] = Left []
+
+occursCheck :: (Matchable concrete' schema' var) => var schema' -> [FreeVar var] -> schema' -> Either (Subst var) (MatchError (var schema) schema)
+occursCheck v []   term | makeTerm v == term      = Left $ []
+occursCheck v args term | v `isMember` (freeVars term) = Right $ ErrWrapper (OccursCheck v term)
+occursCheck v args term                                = Left $ [LambdaMapping v args term]
+
+patternMatch :: (Matchable concrete schema var) => schema -> concrete -> Either (Subst var) (MatchError (var schema) schema)
+patternMatch a b = case (matchVar a b) of
+  Just (LambdaMapping v args tm) -> occursCheck v args tm
+  _ -> case match a b of
+      Just children -> matchChildren children .>. (\e -> SubError e a b)
+      Nothing       -> Right $ UnableToMatch a b
---
+++ Core/Unification/HigherOrderUtil.hs
@@ -0,0 +1,125 @@
+{-#LANGUAGE GADTs, FlexibleInstances, MultiParamTypeClasses, FunctionalDependencies, UndecidableInstances, RankNTypes, TupleSections #-}
+
+
+module HigherOrderUtil (Throne, makeSub, Royal, makeChoice, children) where
+
+import Carnap.Core.Unification.HigherOrderPatternMatching
+import Data.List
+
+--the person that sits on the throne (the highlighted node)
+data Throne concrete schema var where
+   (:~:) :: (Royal schema var) => (var schema, schema) -> (schema, [(schema -> schema, Maybe (schema -> Throne schema var))]) -> Throne schema var
+
+--someone is royal if they can accend to the throne (become a zipper)
+class (Matchable concrete schema var) => Royal schema var where
+    --makes a choice operator out of a set of children
+    makeChoice :: [schema] -> schema
+    --each child should be a pair of the actual child and a function that if given the child forms a new one
+    children :: schema -> ([schmea], [schema] -> schema)
+    --remake the thing out of the children
+
+--defines the order of succession
+--first your child inherits the throne
+--then your siblings inherit the throne if you have no children
+--if you have no younger siblings then your parrents sibling is next in line or their successor if there is none
+--if no such person is next in line then the royal family dies
+successor :: Throne schema var -> [Throne schema var]
+successor ((v, arg) :~: (s, fs)) | not . null . fst . children $ s = [(v, arg) :~: (s, (\s' -> redo $ s':ss, makeNext [] s ss):fs)]
+    where (s:ss, redo) = children s
+          makeNext args king []     = Nothing
+          makeNext args king (x:xs) = let backTrack king' x' = redo $ args ++ [king', x'] ++ xs
+                                          younger king'      = makeNext (args ++ [king']) x xs
+                                      in Just $ \king' -> ((v, arg)) :~: (x, (backTrack king', younger king'):fs)
+                                                            
+successor ((_, _) :~: (s, (_, Just younger):_)) = [younger s]
+successor z = nextInLine z
+
+nextInLine :: Throne schema var -> [Throne schema var]
+nextInLine ((v, arg) :~: (s, (f, Nothing):fs)) = nextInLine $ (v, arg) :~: (f s, fs)
+nextInLine ((_, _) :~: (s, (_, Just younger):_)) = [younger s]
+nextInLine ((_, _) :~: (_, [])) = []
+
+progenitor ((_, _) :~: (s, []))          = s
+progenitor ((v, arg) :~: (s, (f, _):fs)) = progenitor $ (v, arg) :~: (f s, fs)
+
+getSchema ((_, _) :~: (s, _)) = s
+
+makeSub :: (Royal schema var) => Throne schema var -> schema
+makeSub zipper | null $ successor zipper = progenitor zipper
+makeSub zipper@((v, arg) :~: (s, fs)) = case match arg s of
+    Left sub -> let c1 = successor $ (v, apply sub arg) :~: (makeTerm v, fs)
+                    c2 = successor $ (v, arg) :~: (s, fs)
+                in makeChoice $ map makeSub (c1 ++ c2)
+    Right err -> makeSub . head . successor $ zipper
+
+--------------------------------------------------------
+--7. Lets do a diffrent sort of test
+--------------------------------------------------------
+
+{-
+data Term = Term String
+data FormulaVar a where
+    FormulaVar :: String -> FormulaVar Formula
+    TermVar :: String -> FormulaVar Term
+
+--these are simple types
+data Formula = Connective String [Formula]
+             | Predicate String [FormulaVar Term]
+    deriving(Eq, Show)
+
+instance Show (FormulaVar a) where
+    show (FormulaVar s) = s
+    show (TermVar s) = s
+
+instance EquaitableVar FormulaVar where
+  getLikeSchema (FormulaVar v) (FormulaVar v') s' | v == v' = Just s'
+  getLikeSchema _              _               _            = Nothing
+
+instance Eq (FormulaVar a) where
+    a == b = eq a b
+
+--first lets do types 
+instance MultiMatchable Formula FormulaVar where
+    multiMatch (Connective v args)  (Connective v' args') 
+        | v == v' && length args == length args'  = Just $ eZip args args'
+        where eZip (x:xs) (y:ys) = (UnifiablePairing x y) : (eZip xs ys)
+              eZip []     _      = []
+              eZip _      []     = []
+    multiMatch (Predicate a args) expr            = Just []
+    multiMatch expr (Predicate a args)            = Just []
+
+--I think these two functions are cheating
+--I do not belive you could write these for somthing more complex with multiple varible types
+--I'll try and create a more general version after I have this basic higher order version down
+convert :: FormulaVar schema -> FormulaVar Formula
+convert (FormulaVar s) = FormulaVar s
+
+convertTerm :: FormulaVar schema -> schema -> Formula
+convertTerm v s = case getLikeSchema (convert v) v s of
+    Just s' -> s'
+
+instance MultiHilbert Formula FormulaVar where
+    multiFreeVars (Connective v args) = foldl union [] (map multiFreeVars args) 
+    multiFreeVars (Predicate v  args) = [FreeVar (FormulaVar v)] ++ map FreeVar args
+
+    --multiApply = undefined
+    multiApply subst (Connective v args) = Connective v (map (multiApply subst) args)
+    multiApply subst (Predicate  v )   = case fvMapLookup (FormulaVar v) subst of
+        Just (Mapping v' tm) -> convertTerm v' tm
+        Nothing              -> Predicate  v []
+    multiApply subst (Predicate  v args) = case fvMapLookup (FormulaVar v) subst of
+        Just (LambdaMapping v vargs tm) -> applyEach (convertTerm v tm) (zip vargs args)
+        Nothing                         -> Predicate v args
+        where applyEach :: Formula -> [(FreeVar FormulaVar, Formula)] -> Formula
+              applyEach tm (((FreeVar v'), arg):xs) = applyEach (multiApply [Mapping (convert v') arg] tm) xs
+
+--I'm not done with this yet
+instance MultiUnifiable Formula FormulaVar where
+    multiMatchVar (Predicate v vars) term = undefined
+    multiMakeTerm = undefined
+    --multiMatchVar (Predicate v args) tm = Just $ Mapping v tm
+    --multiMatchVar tm (Predicate v args) = Just $ Mapping v tm
+    --multiMatchVar _  _                  = Nothing
+
+    --multiMakeTerm = TyVar
+    -}
