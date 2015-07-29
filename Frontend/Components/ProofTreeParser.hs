779f30d7502d54e3e6520bb12212796b0091cf8c
gleachkr <gleachkr@ksu.edu>
2015-07-29 13:52:26 -0500

    Made ProofTreeParser language agnostic.

--- Frontend/Components/ProofTreeParser.hs
+++ Frontend/Components/ProofTreeParser.hs
@@ -7,21 +7,26 @@ import Text.Parsec as P
 import Data.Tree
 
 --The goal of this module is to provide a function which transforms a given
---(well-formed) string into a ProofTree
---
+--(well-formed) string into a ProofTree. We're going to be passing around
+--a formula parser.
+
 --------------------------------------------------------
 --1. Functions for parsing strings into ProofTrees
 --------------------------------------------------------
 
-parseTheBlock :: String -> Either ParseError (ProofForest, Termination)
-parseTheBlock = parse blockParser ""
+type FParser f = Parsec String () f 
+
+parseTheBlock :: Show f => FParser f -> String -> Either ParseError (ProofForest f, Termination)
+parseTheBlock fParser = parse (blockParser fParser) ""
 --parses a string into a proof forest, and a termination (returning SHOW
 --when no termination is found), by repeatedly grabbing standard and show
 --lines, and then checking for a termination line
-blockParser:: Parsec String st (ProofForest,Termination)
-blockParser = do block <- P.many1 $ try getStandardLine P.<|> try getShowLine P.<|> try getErrLine
-                 termination <- try getTerminationLine P.<|> return ("SHOW",[])
-                 return (block,termination)
+blockParser:: Show f => FParser f -> Parsec String () (ProofForest f,Termination)
+blockParser fParser = do block <- P.many1 $ try (getStandardLine fParser)
+                                            P.<|> try (getShowLine fParser)
+                                            P.<|> try (getErrLine fParser)
+                         termination <- try getTerminationLine P.<|> return ("SHOW",[])
+                         return (block,termination)
 
 --gathers to the end of an intented block
 getIndentedBlock :: Parsec String st String 
@@ -31,44 +36,45 @@ getIndentedBlock = tab >> P.manyTill anyChar (try hiddenEof P.<|> try endOfInden
 --the results.
 --
 --XXX:this involves multiple passes, and could probably be streamlined.
-processIndentedBlock :: Parsec String st (ProofForest,Termination)
-processIndentedBlock = do x <- getIndentedBlock 
-                          let y = parse stripTabs "" x
-                          let forestAndTerm = parse blockParser "" $ stringHandler y
-                          return $ pairHandler forestAndTerm
+processIndentedBlock :: Show f => FParser f -> Parsec String st (ProofForest f,Termination)
+processIndentedBlock fParser = do x <- getIndentedBlock 
+                                  let y = parse stripTabs "" x
+                                  let forestAndTerm = parse (blockParser fParser) "" $ stringHandler y
+                                  return $ pairHandler forestAndTerm
 
-getErrLine :: Parsec String st ProofTree
-getErrLine = do l <- P.many1 (noneOf ['\n','\t'])
-                _ <- newline
-                case parse formulaParser "" l of
-                   Left e -> return $ Node (Left $ show e) []
-                   Right f -> return $ Node (Left $ show f) []
- 
+getErrLine :: Show f => FParser f -> Parsec String st (ProofTree f)
+getErrLine fParser = do l <- P.many1 (noneOf ['\n','\t'])
+                        _ <- newline
+                        case parse fParser "" l of
+                            Left e -> return $ Node (Left $ show e) []
+                            Right f -> return $ Node (Left $ show f) []
+
 --Consumes a show line and a subsequent intented block, and returns a tree
 --with the contents of the show line at the root (with the SHOW rule to
 --indicate an incomplete subproof, and otherwise with the rule used to
 --terminate the subproof) , and the contents of the indented subderivation
 --below
-getShowLine :: Parsec String st ProofTree
-getShowLine = do _ <- oneOf "sS"
-                 skipMany (alphaNum P.<|> char ':')
-                 blanks
-                 f <- formulaParser
-                 _ <- newline
-                 (subder,(rule,lns)) <- try processIndentedBlock P.<|> return ([],("SHOW",[]))
-                 return $ Node (Right (f, rule, lns)) subder
+getShowLine :: Show f=> FParser f -> Parsec String () (ProofTree f)
+getShowLine fParser = do _ <- oneOf "sS"
+                         skipMany (alphaNum P.<|> char ':')
+                         blanks
+                         f <- fParser
+                         _ <- newline
+                         (subder,(rule,lns)) <- try (processIndentedBlock fParser)
+                                                P.<|> return ([],("SHOW",[]))
+                         return $ Node (Right (f, rule, lns)) subder
 
 --Consumes a standard line, and returns a single node with that assertion
 --and its justification
-getStandardLine :: Parsec String st ProofTree
-getStandardLine   = do f <- formulaParser
-                       blanks
-                       r <- inferenceRuleParser
-                       blanks
-                       l <- try lineListParser P.<|> return []
-                       let l' = Prelude.map read l :: [Int]
-                       _ <- newline
-                       return $ Node (Right (f,r,l')) []
+getStandardLine :: FParser f -> Parsec String () (ProofTree f)
+getStandardLine fParser = do f <- fParser
+                             blanks
+                             r <- inferenceRuleParser
+                             blanks
+                             l <- try lineListParser P.<|> return []
+                             let l' = Prelude.map read l :: [Int]
+                             _ <- newline
+                             return $ Node (Right (f,r,l')) []
 
 --Consumes a termination line, and returns the corresponding termination
 getTerminationLine :: Parsec String st Termination
