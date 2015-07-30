5472f68f4f72312837930eb8b3605e0d0dcfdb31
gleachkr <gleachkr@ksu.edu>
2015-07-30 16:02:46 -0500

    Fix an accidental reversion

--- Languages/FirstOrder/QuantifiedLanguage.hs
+++ Languages/FirstOrder/QuantifiedLanguage.hs
@@ -6,7 +6,10 @@ import Carnap.Core.Data.AbstractSyntaxDataTypes
 import Carnap.Core.Data.AbstractSyntaxMultiUnification()
 import Carnap.Core.Data.AbstractSyntaxSecondOrderMatching
 import Carnap.Core.Unification.HigherOrderMatching
+import Carnap.Core.Unification.HigherOrderUtil
 import Carnap.Languages.Util.LanguageClasses
+
+import Control.Lens
 
 --------------------------------------------------------
 --1. Data types for the language of First Order Logic, with equality
@@ -308,7 +311,8 @@ instance SItemConstants QItem where
     delta n = SeqVar (SideForms $ "Δ_" ++ show n)
 
 instance S_NextVar Referent FirstOrderQuantifiers where
-        s_appropriateVariable f _ = "x_" ++ show (s_quantifierCount f)
+        s_appropriateVariable f _ = "x_" ++ show (1 + s_maxBlankForm f)
+
 
 folMatch :: FirstOrderScheme -> FirstOrderScheme -> Either (MatchError (Qvar FirstOrderScheme) FirstOrderScheme) [Subst Qvar] 
 folMatch = patternMatch
@@ -319,6 +323,24 @@ folMatch = patternMatch
 
 instance UniformlyEquaitable Nothing where 
         eq = (=*)
+
+s_maxBlankTerm :: SchematicTerm pred con FirstOrderQuantifiers f Referent () -> Int
+s_maxBlankTerm (S_BlankTerm (_:_:n)) = read n
+s_maxBlankTerm node                  = composOpFold 0 max s_maxBlankTerm node
+
+--this one is, for some reason, hard to implement with composOpFold or other lens-tech
+s_maxBlankForm :: SchematicForm pred con FirstOrderQuantifiers f Referent a -> Int
+s_maxBlankForm (S_Bind _ f) = s_maxBlankForm (f $ BlankTerm "*")
+s_maxBlankForm (S_SchematicBind _ f) = s_maxBlankForm (f $ BlankTerm "*")
+s_maxBlankForm (S_UnaryConnect _ f) = s_maxBlankForm f
+s_maxBlankForm (S_UnarySchematicConnect _ f) = s_maxBlankForm f
+s_maxBlankForm (S_BinaryConnect _ f g) = s_maxBlankForm f `max` s_maxBlankForm g
+s_maxBlankForm (S_BinarySchematicConnect _ f g) = s_maxBlankForm f `max` s_maxBlankForm g
+s_maxBlankForm (S_UnaryPredicate _ f) = s_maxBlankTerm f
+s_maxBlankForm (S_UnarySchematicPredicate _ f) = s_maxBlankTerm f
+s_maxBlankForm (S_BinaryPredicate _ f g) = s_maxBlankTerm f `max` s_maxBlankTerm g
+s_maxBlankForm (S_BinarySchematicPredicate _ f g) = s_maxBlankTerm f `max` s_maxBlankTerm g
+s_maxBlankForm _ = 0
 
 quantifierCount :: Form pred con quant f sv a -> Int
 quantifierCount (Bind _ f) = quantifierCount (f $ BlankTerm "*") + 1
@@ -359,3 +381,18 @@ substitute (BinaryConnect If f1 f2) t1 t2 = BinaryConnect If (substitute f1 t1 t
 substitute (BinaryConnect Iff f1 f2) t1 t2 = BinaryConnect Iff (substitute f1 t1 t2) (substitute f2 t1 t2)
 substitute (Bind Universal f) t1 t2 = Bind Universal (\x -> substitute (f x) t1 t2)
 substitute (Bind Existential f) t1 t2 = Bind Existential (\x -> substitute (f x) t1 t2)
+
+--if you defined plated and multiplated as I did here
+instance Plated FirstOrderTerm where
+    plate = undefined
+
+instance MultiPlated FirstOrderFormula FirstOrderTerm where
+    multiplate = undefined
+
+--this should work assuming the above are implemented
+--also I think this is a generic function over any multiplated thing where the child type implements Eq!!
+--so like everything basically
+substitute' :: FirstOrderFormula -> FirstOrderTerm -> FirstOrderTerm -> FirstOrderFormula
+substitute' node t1 t2 = rewriteOnOf multiplate id replace node
+    where replace c | c == t1   = Just t2
+                    | otherwise = Nothing
