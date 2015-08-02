e0d3b7301d6da0785f282d06c9b62cf8eecdad6c
JakeEhrlich <jake.h.ehrlich@gmail.com>
2015-08-02 16:02:05 -0500

    Ok deleting a BUNCH of stuff now (HASTE + old systems and languages)

--- Frontend/Haste/Implementations/ClassicalSLKalishAndMontegueEmbedding.hs
+++
@@ -1,6 +0,0 @@
---{-# LANGUAGE  OverloadedStrings, DeriveDataTypeable #-} 
-module Main where
-import Carnap.Frontend.Haste.Components.ProofPadEmbedding
-import Carnap.Calculi.ClassicalSententialLogic1 (classicalRules, classicalSLruleSet)
-
-main = embedWith (classicalRules, classicalSLruleSet)
