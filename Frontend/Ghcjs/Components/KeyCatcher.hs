5104adbf69dae2c9a0d1e8b306a284214347a1c8
gleachkr <gleachkr@ksu.edu>
2015-08-07 11:56:15 -0600

    Added support for keypress intercept.

--- Frontend/Ghcjs/Components/KeyCatcher.hs
+++ Frontend/Ghcjs/Components/KeyCatcher.hs
@@ -3,13 +3,14 @@ module Carnap.Frontend.Ghcjs.Components.KeyCatcher where
 
 import Control.Monad.Reader
 import Data.IORef
-import GHCJS.DOM.Element (elementOnkeyup)
+import GHCJS.DOM.Element (elementOnkeypress)
 import GHCJS.DOM.HTMLElement (castToHTMLElement)
-import GHCJS.DOM.EventM(uiKeyCode)
+import GHCJS.DOM.EventM(uiCharCode, preventDefault)
 
-keyCatcher :: System.Glib.Types.GObjectClass obj => obj -> (IORef [t] -> Int -> IO ()) -> IO ()
+--keyCatcher :: System.Glib.Types.GObjectClass obj => obj -> (IORef [t] -> Int -> IO ()) -> IO ()
 keyCatcher target handler = do let el = castToHTMLElement target
                                kbuffer <- newIORef [] 
-                               elementOnkeyup el $ do k <- uiKeyCode
-                                                      liftIO $ handler kbuffer k
+                               elementOnkeypress el $ do k <- uiCharCode
+                                                         catch <- liftIO $ handler kbuffer k
+                                                         if catch then preventDefault else return ()
                                return ()
