074c200422c355c3ade36fd0fa67bf5fd57b7b7a
gleachkr <gleachkr@ksu.edu>
2015-08-14 09:40:45 -0500

    Copyright notices in headers. Closes #25.

--- Core/Data/AbstractSyntaxMultiUnification.hs
+++ Core/Data/AbstractSyntaxMultiUnification.hs
@@ -1,5 +1,21 @@
 {-#LANGUAGE GADTs, TypeSynonymInstances, FlexibleInstances, FlexibleContexts, MultiParamTypeClasses#-}
+{-Copyright 2015 (C) Jake Ehrlich and Graham Leach-Krouse
 
+This file is part of Carnap. 
+
+Carnap is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Carnap is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Carnap If not, see <http://www.gnu.org/licenses/>.
+-}
 module Carnap.Core.Data.AbstractSyntaxMultiUnification where
 
 import Carnap.Core.Unification.MultiUnification
