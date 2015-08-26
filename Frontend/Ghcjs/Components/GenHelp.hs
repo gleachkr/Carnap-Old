8eae627cb0c5775416d8daa8ad14032c1330c5b7
gleachkr <gleachkr@ksu.edu>
2015-08-26 11:45:46 -0500

    Big improvement to help popup.

--- Frontend/Ghcjs/Components/GenHelp.hs
+++ Frontend/Ghcjs/Components/GenHelp.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {- Copyright (C) 2015 Jake Ehrlich and Graham Leach-Krouse <gleachkr@ksu.edu>
 
 This file is part of Carnap 
@@ -16,18 +17,51 @@ You should have received a copy of the GNU General Public License
 along with Carnap. If not, see <http://www.gnu.org/licenses/>.
 - -}
 
-module Carnap.Frontend.Ghcjs.Components.GenHelp (ruleTable) where
+module Carnap.Frontend.Ghcjs.Components.GenHelp (ruleTable, inferenceTable, terminationTable) where
 
 import Carnap.Core.Data.AbstractDerivationDataTypes (RulesAndArity)
-import Carnap.Core.Data.Rules (Sequent(Sequent), AbsRulePlus(rule), AbsRule(AbsRule),AmbiguousRulePlus(ruleVersionsPlus,ruleNamePlus))
-import Data.Set (toList)
+import Carnap.Core.Data.Rules (AbsRulePlus(rule), AbsRule(),AmbiguousRulePlus(ruleVersionsPlus,ruleNamePlus))
+import Data.Set as S (Set(Set), toList, filter) 
 import Text.Blaze.Html5 as B
 --import Text.Blaze.Html5.Attributes
-import Text.Blaze.Internal (stringValue, MarkupM)
+import Text.Blaze.Internal ()
 import Data.Monoid (mconcat, (<>))
+import Data.Map.Strict as M (lookup)
+import Data.Map (Map)
 
-ruleTable rs = table $ mconcat $ Prelude.map ruleRow $ toList rs
+ruleTable comments rs = table $ mconcat $ Prelude.map (ruleRow comments) $ toList rs
 
-ruleRow ambrp = tr $ td (toHtml $ ruleNamePlus ambrp) <> mconcat (ruleCols ambrp) 
+ruleRow comments ambrp = case M.lookup name comments of
+                             Just comment -> tr $ td (toHtml name <> toHtml (": " ++ comment)) <> ruleCols ambrp
+                             Nothing -> tr $ td (toHtml name) <> ruleCols ambrp 
+                        where name = ruleNamePlus ambrp
 
-ruleCols ambrp = Prelude.map (td . toMarkup . rule) (ruleVersionsPlus ambrp)
+ruleCols ambrp = do mconcat $ Prelude.map (td . toMarkup . rule) $ Prelude.head chunks
+                    mapM_ (\x -> tr $ td (toHtml "ctd.") <> 
+                           mconcat (Prelude.map (td . toMarkup . rule) x)) 
+                           (tail chunks)
+            where chunks = chunksOf 3 (ruleVersionsPlus ambrp)
+
+chunksOf :: Int -> [e] -> [[e]]
+chunksOf i ls = Prelude.map (take i) (build (splitter ls)) where
+  splitter :: [e] -> ([e] -> a -> a) -> a -> a
+  splitter [] _ n = n
+  splitter l c n  = l `c` splitter (drop i l) c n
+  build g = g (:) []
+
+
+terminationRules :: Set (AmbiguousRulePlus a b) -> RulesAndArity -> Set (AmbiguousRulePlus a b)
+terminationRules rs raa = S.filter (\x -> case raa (ruleNamePlus x) of 
+                                        Just (Right _) -> True 
+                                        _ -> False) rs
+
+inferenceRules :: Set (AmbiguousRulePlus a b) -> RulesAndArity -> Set (AmbiguousRulePlus a b)
+inferenceRules rs raa = S.filter (\x -> case raa (ruleNamePlus x) of 
+                                        Just (Left _) -> True 
+                                        _ -> False) rs
+
+inferenceTable :: ToMarkup (AbsRule a) => Set (AmbiguousRulePlus a b) -> RulesAndArity -> Map String String -> Html
+inferenceTable rs raa comments = ruleTable comments $ inferenceRules rs raa
+
+terminationTable :: ToMarkup (AbsRule a) => Set (AmbiguousRulePlus a b) -> RulesAndArity -> Map String String -> Html
+terminationTable rs raa comments = ruleTable comments $ terminationRules rs raa
--- Frontend/Ghcjs/Implementations/FOLBox/src/Main.hs
+++ Frontend/Ghcjs/Implementations/FOLBox/src/Main.hs
@@ -20,14 +20,17 @@ module Main (
     main
 ) where
 
+import Data.Map as M
+import Data.Monoid ((<>))
 import Carnap.Calculi.ClassicalFirstOrderLogic1 (classicalRules, classicalQLruleSet, prettyClassicalQLruleSet)
 import Carnap.Frontend.Ghcjs.Components.ActivateProofBox
 import Carnap.Frontend.Ghcjs.Components.KeyCatcher
-import Carnap.Frontend.Ghcjs.Components.GenHelp (ruleTable)
+import Carnap.Frontend.Ghcjs.Components.GenHelp (inferenceTable, terminationTable)
 import Carnap.Frontend.Ghcjs.Components.GenPopup (genPopup)
 import Carnap.Languages.FirstOrder.QuantifiedParser (formulaParser)
 import Control.Applicative ((<$>))
 import Control.Monad.Trans (liftIO)
+import Text.Blaze.Html5 as B
 import GHCJS.DOM.Node (nodeInsertBefore)
 import GHCJS.DOM.Element (elementSetAttribute, elementOnclick, elementFocus)
 import GHCJS.DOM.Types (HTMLDivElement, HTMLElement)
@@ -49,7 +52,7 @@ main = runWebGUI $ \webView -> do
     return ()
     where byCase doc (n,l) = case n of 
             Just node -> do activateProofBox node doc classicalRules classicalQLruleSet formulaParser
-                            help <- genPopup node doc (ruleTable prettyClassicalQLruleSet) ("help" ++ show l)
+                            help <- genPopup node doc helpPopup ("help" ++ show l)
                             keyCatcher node $ \kbf k -> do if k == 63 then do elementSetAttribute help "style" "display:block" 
                                                                               elementFocus help
                                                                       else return ()
@@ -57,6 +60,46 @@ main = runWebGUI $ \webView -> do
                             return ()
             Nothing -> return ()
 
-
 nodelistToNumberedList nl = do len <- nodeListGetLength nl
                                mapM (\n -> do i <- nodeListItem nl n; return (i,n)) [0 .. len]
+
+helpPopup = B.div (toHtml infMessage) <>
+            inferenceTable prettyClassicalQLruleSet classicalRules comments <>
+            B.div (toHtml termMessage) <>
+            terminationTable prettyClassicalQLruleSet classicalRules comments
+
+infMessage :: String
+infMessage = "The follow are inference rules. They can be used to directly justify the assertion on a given line, by referring to previous open lines."
+     <> " An inference rule can justify a statement matching the form that appears on the right side of the sequent that concludes the rule."
+     <> " (i.e. on the right side of the \"⊢\" following the \"∴\".)"
+     <> " It needs to refer back to previous lines which match all of the forms that appear on the right side of the sequents in the premises of the rule."
+     <> " The symbols on the left sides of the sequents tell you how the dependencies of the justified line related to the dependencies of the lines that justify it."
+
+termMessage :: String
+termMessage = "the following are termination rules. They can be used to close a subproof, by referring to previous open lines (including lines that belong to the subproof)."
+      <> " A termination rule can close a subproof that begins with a show line followed by something matching the form that appears on the right side of the sequent that concludes the rule."
+      <> " It needs to refer back to previous lines which match all of the forms that appear on the right side of the sequents in the premises of the rule."
+      <> " The symbols on the left sides of the sequents tell you how the dependencies of the statement established by the subproof relate to the dependencies of the lines that close the subproof."
+
+comments = M.fromList [
+                      ("R","Reflexivity"),
+                      ("BC", "Biconditional to conditional"),
+                      ("IE", "Interchange of Equivalents"),
+                      ("S", "Simplification"),
+                      ("CB", "Conditional to Biconditional"),
+                      ("MTP", "Modus Tollendo Ponens"),
+                      ("DN", "Double Negation"),
+                      ("MT", "Modus Tollens"),
+                      ("LL", "Leibniz's Law"),
+                      ("EG", "Existential Generalization"),
+                      ("ADD", "Addition"),
+                      ("MP", "Modus Ponens"),
+                      ("ADJ", "Adjunction"),
+                      ("UI", "Universal Instantiation"),
+                      ("UD", "Universal Derivation"),
+                      ("ED", "Existential Derivation"),
+                      ("ID", "Indirect Derivation"),
+                      ("CD", "Conditional Derivation"),
+                      ("DD", "Direct Derivation")
+                      ]
+
